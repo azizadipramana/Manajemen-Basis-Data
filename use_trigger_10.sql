899d774ea87b184ff9bb3d361c14de1a6468fa77
Shi-Miyuki <aziz.adipramana24@gmail.com>
2026-05-15 14:55:49 +0700

    Create use_trigger_10.sql

---
+++ use_trigger_10.sql
@@ -0,0 +1,135 @@
+-- use trigger 1: cek_izin_zona
+
+-- contoh berhasil 
+
+INSERT INTO transaksi_parkir (
+    id_transaksi,
+    waktu_masuk,
+    waktu_keluar,
+    id_kendaraan,
+    id_petugas,
+    id_zona
+)
+VALUES (
+    21,
+    '2026-03-06 08:00:00',
+    '2026-03-06 11:00:00',
+    2,
+    1,
+    2
+);
+
+-- contoh gagal 
+
+INSERT INTO transaksi_parkir (
+    id_transaksi,
+    waktu_masuk,
+    waktu_keluar,
+    id_kendaraan,
+    id_petugas,
+    id_zona
+)
+VALUES (
+    22,
+    '2026-03-06 08:00:00',
+    '2026-03-06 10:00:00',
+    2,
+    1,
+    1
+);
+
+
+-- use trigger 2: 
+
+-- contoh berhasil 
+INSERT INTO transaksi_parkir (
+    id_transaksi,
+    waktu_masuk,
+    waktu_keluar,
+    id_kendaraan,
+    id_petugas,
+    id_zona
+)
+VALUES (
+    23,
+    '2026-03-06 08:00:00',
+    '2026-03-06 12:00:00',
+    1,
+    1,
+    1
+);
+
+-- contoh gagal 
+
+INSERT INTO transaksi_parkir (
+    id_transaksi,
+    waktu_masuk,
+    waktu_keluar,
+    id_kendaraan,
+    id_petugas,
+    id_zona
+)
+VALUES (
+    24,
+    '2026-03-06 12:00:00',
+    '2026-03-06 10:00:00',
+    1,
+    1,
+    1
+);
+
+-- use trigger 3: 
+
+-- update size 
+UPDATE zona_parkir
+SET kapasitas = 1
+WHERE id_zona = 1;
+
+-- contoh berhasil 
+
+INSERT INTO transaksi_parkir VALUES
+(
+    25,
+    '2026-03-06 08:00:00',
+    '2026-03-06 10:00:00',
+    1,
+    1,
+    1
+);
+
+-- contoh gagal 
+
+INSERT INTO transaksi_parkir VALUES
+(
+    26,
+    '2026-03-06 08:30:00',
+    '2026-03-06 11:00:00',
+    6,
+    2,
+    1
+);
+
+-- trigger 4: 
+
+INSERT INTO transaksi_parkir (
+    id_transaksi,
+    waktu_masuk,
+    waktu_keluar,
+    id_kendaraan,
+    id_petugas,
+    id_zona
+)
+VALUES (
+    27,
+    '2026-03-06 08:00:00',
+    '2026-03-06 10:30:00',
+    1,
+    1,
+    1
+);
+
+-- cek hasil 
+
+SELECT *
+FROM transaksi_parkir
+WHERE id_transaksi = 27;
